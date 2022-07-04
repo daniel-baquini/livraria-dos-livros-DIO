929b9e0b0fe02ef6851e38ce5b37f0e74f79083a
daniel-baquini <danielbaquini@protonmail.com>
2022-07-04 00:50:10 -0300

    Added style and markup of the 'login-page'. Also addeded the app fonts (gothic a1 and robot)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        loadChildren: () => import("./authentication/authentication.module").then(m => m.AuthenticationModule),
+        path: ''
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    exports: [
+        RouterModule
+    ],
+    imports: [
+        RouterModule.forRoot(routes)
+    ]
 })
 export class AppRoutingModule { }
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginPageComponent } from './login-page/login-page.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
-
+const authenticationRoutes: Routes = [
+    {
+        component: LoginPageComponent,
+        path: ''
+    }
+]
 
 @NgModule({
-  declarations: [
-    LoginPageComponent,
-    CreateAccountComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        LoginPageComponent,
+        CreateAccountComponent
+    ],
+    imports: [
+        RouterModule.forChild(authenticationRoutes),
+        SharedModule
+    ]
 })
 export class AuthenticationModule { }
