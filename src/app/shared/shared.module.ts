0d93930bb9ccd68870fde4ffcc519c1c6fd757c0
daniel-baquini <danielbaquini@protonmail.com>
2022-07-04 00:01:29 -0300

    Added the component 'field', based on figma component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { BookPreviewComponent } from './book-preview/book-preview.component';
 import { ButtonComponent } from './button/button.component';
 import { CartItemComponent } from './cart-item/cart-item.component';
 import { CommonModule } from '@angular/common';
+import { FieldComponent } from './field/field.component';
 import { LogoComponent } from './logo/logo.component';
 import { NavComponent } from './nav/nav.component';
 import { NgModule } from '@angular/core';
@@ -14,15 +15,17 @@ import { SmallButtonComponent } from './small-button/small-button.component';
         BookPreviewComponent,
         ButtonComponent,
         CartItemComponent,
+        FieldComponent,
         LogoComponent,
         NavComponent,
-        SmallButtonComponent
+        SmallButtonComponent,
     ],
     exports: [
         AlertComponent,
         BookPreviewComponent,
         ButtonComponent,
         CartItemComponent,
+        FieldComponent,
         LogoComponent,
         NavComponent,
         SmallButtonComponent
