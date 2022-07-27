77a741c9a6b8462d321271e209e2d274cb5e5af8
daniel-baquini <danielbaquini@protonmail.com>
2022-07-27 09:08:26 -0300

    Added 'forgot-password-page' based on figma's prototype

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,4 +1,5 @@
 import { CreateAccountPageComponent } from './create-account-page/create-account-page.component';
+import { ForgotPasswordPageComponent } from './forgot-password-page/forgot-password-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -12,12 +13,17 @@ const authenticationRoutes: Routes = [
     {
         component: CreateAccountPageComponent,
         path: "create-account"
+    },
+    {
+        component: ForgotPasswordPageComponent,
+        path: "forgot-password"
     }
 ];
 
 @NgModule({
     declarations: [
         CreateAccountPageComponent,
+        ForgotPasswordPageComponent,
         LoginPageComponent
     ],
     imports: [
