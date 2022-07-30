ca725df3570af9173feb04bfd49750ca0b5ff893
daniel-baquini <danielbaquini@protonmail.com>
2022-07-29 21:36:10 -0300

    Finished the JWT authentication implementation.

--- back-end/src/main/java/com/backend/config/SecurityConfiguration.java
+++ back-end/src/main/java/com/backend/config/SecurityConfiguration.java
@@ -4,36 +4,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.backend.security.JWTFilter;
 import com.backend.service.SecurityDatabaseService;
 
 @Configuration
+// @EnableWebSecurity
+// @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     
     @Autowired
     private SecurityDatabaseService securityService;
 
-    @Autowired
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(securityService).passwordEncoder(new BCryptPasswordEncoder());
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.headers().frameOptions().disable();
+        // Authentication for creation, delete and update will be made with a JWT token,
+        // so cors and csrf is not required.
+        http.cors().and().csrf().disable()
+            .addFilterAfter(new JWTFilter(), UsernamePasswordAuthenticationFilter.class)
+            .authorizeRequests()
+        //http.authorizeHttpRequests()
             .antMatchers("/").permitAll()
             .antMatchers("/app", "/app/**").permitAll()
             .antMatchers("/api/book", "/api/book/**").permitAll()
             .antMatchers("/api/user", "/api/user/**").permitAll()
             .antMatchers("/images", "/images/**").permitAll()
-            .anyRequest().authenticated().and().httpBasic();
-        http.csrf().disable();
+            .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
+    }
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Bean
--- back-end/src/main/java/com/backend/security/JWTFilter.java
+++ back-end/src/main/java/com/backend/security/JWTFilter.java
@@ -1,5 +1,69 @@
 package com.backend.security;
 
-public class JWTFilter {
-    
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.backend.config.JWTConfiguration;
+import com.backend.config.SecurityConfiguration;
+import com.backend.model.JWTObject;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+// All requests will be intercepted by this filter, in the "doFilterInternal",
+// we will check for a valid JWT.
+public class JWTFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = request.getHeader(JWTCreator.HEADER_AUTHORIZATION);
+        try {
+            if(token != null && !token.isEmpty()) {
+                JWTObject tokenObject = JWTCreator.create(token, JWTConfiguration.PREFIX, JWTConfiguration.KEY);
+                List<SimpleGrantedAuthority> authorities = authorities(tokenObject.getRoles());
+
+                var userToken = new UsernamePasswordAuthenticationToken(
+                    tokenObject.getSubject(),
+                    null,
+                    authorities
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(userToken);
+            }
+            else {
+                SecurityContextHolder.clearContext();
+            }
+            filterChain.doFilter(request, response);
+        }
+        catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
+            // TODO: see if it's possible to return a body, containing the description of the error.
+            e.printStackTrace();
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return;
+        }
+    }
+
+    private List<SimpleGrantedAuthority> authorities(List<String> roles) {
+        return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+
 }
+
+
